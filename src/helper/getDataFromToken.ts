a8bc9eda7cbb6bbf6ac342de4ee3e702ab7d6e3b
Sonali Kanawade <sonalikanawade111@gmail.com>
2025-02-28 22:04:34 +0530

    added

--- src/helper/getDataFromToken.ts
+++ src/helper/getDataFromToken.ts
@@ -1,11 +1,11 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 import jwt from 'jsonwebtoken';
 
 export async function  getDataFromToken(request: NextRequest) {
     try {
         const token = request.cookies.get('token')?.value || '';
     
-        const decoedToken = await jwt.verify(token, 'mynexttoken');
+        const decoedToken: any = await jwt.verify(token, 'mynexttoken');
         return decoedToken.id;
     } catch (error: any) {        
         throw new Error(error.message);
